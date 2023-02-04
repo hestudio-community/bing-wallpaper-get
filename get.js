d55686616d7d4defdf98750646534d5355633022
醉、倾城 <zihengdie@hestudio.org>
2023-02-04 17:43:05 +0000

    1/2 release

--- get.js
+++ get.js
@@ -1,12 +1,31 @@
-const { default: axios } = require('axios');
+// const fs = require('fs');
 const express = require('express')
-const request = require('request')
+const cron = require("node-cron");
+const path = require('path');
+const shell = require("shelljs");
+// const request = require('request')
 const app = express()
 const port = 3000
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
+function downimg() {
+  const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN');
+    xhr.send();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        var r = xhr.responseText;
+        var bingsrc = JSON.parse(r);
+        var url = "https://cn.bing.com"+bingsrc.images[0].url
+        shell.exec('wget -O image.jpg '+url)
+      }
+    }
+  }
+
+
 // 允许跨域
-app.all('*', function(req, res, next) {
+app.all('*', function(req,
+  res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
@@ -16,26 +35,8 @@ app.all('*', function(req, res, next) {
 });
  
 app.get('/getimage', (req, res) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN');
-    xhr.send();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        var r = xhr.responseText;
-        var bingsrc = JSON.parse(r);
-        let url = "https://cn.bing.com"+bingsrc.images[0].url
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-          //拿到实际请求返回的响应头，根据具体需求来设置给原来的响应头
-              let headers = response.headers;
-              res.setHeader('content-type',headers['content-type']);
-              res.send(body);
-            }
-	   
-          });
-        }
-      
-    }
+  downimg()
+  res.sendFile(path.join(__dirname, 'image.jpg'))
 })
 app.listen(port, () => {
   console.log(`heStudio BingWallpaper Get is running on localhost:${port}`)
