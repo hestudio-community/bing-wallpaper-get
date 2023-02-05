bf8dd6d5a04b1a2b1fcd33df768a7517adb87e2a
醉、倾城 <zihengdie@hestudio.org>
2023-02-05 15:21:44 +0000

    1.1.0 更新版权信息和标题输出

--- README.md
+++ README.md
@@ -1,18 +1,40 @@
 # heStudio BingWallpaper Get
 
-### demo
-![这是一张自动更换的图片](https://hbwg.hestudio.org/getimage)
+### Demo
+详见 https://www.hestudio.org/docs/hestudio_bing_wallpaper_get.html
 
 ### 调用方法
-运行端口在`3000`
+> 运行端口在`3000`
+#### 获取图片
 ```
 GET /getimage
 ```
 
+#### 获取图片标题
+```
+Get /gettitle
+```
+
+返回参数：
+| 参数 | 说明 |
+|---|---|
+| `title` | 标题 |
+
+#### 获取图片版权信息
+```
+GET /getcopyright
+```
+
+返回参数：
+| 参数 | 说明 |
+|–––|–––|
+| `copyright` | 版权信息 |
+| `copyrightlink` | 版权信息所对应的链接 |
+
 ### 安装方法
 #### Docker部署 (推荐)
 ```shell
-docker pull registry.cn-hangzhou.aliyuncs.com/hestudio/hestudio_bing_wallpaper_get:1.0.3
+docker pull registry.cn-hangzhou.aliyuncs.com/hestudio/hestudio_bing_wallpaper_get:1.1.0
 ```
 #### NPM安装
 ```shell
--- get.js
+++ get.js
@@ -1,45 +1,61 @@
-const express = require('express')
-const CronJob = require('cron').CronJob
+const express = require('express');
+const CronJob = require('cron').CronJob;
 const path = require('path');
 const shell = require("shelljs");
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-function downimg() {
+function cacheimg() {
   const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN');
-    xhr.send();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        var r = xhr.responseText;
-        var bingsrc = JSON.parse(r);
-        var url = "https://cn.bing.com"+bingsrc.images[0].url
-        shell.exec('wget -O image.jpg '+url)
-      }
-    }
-  }
+  xhr.open('GET', 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN');
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var r = xhr.responseText;
+      var bingsrc = JSON.parse(r);
+      var url = "https://cn.bing.com" + bingsrc.images[0].url;
+      shell.exec('wget -O image.jpg ' + url);
+      global.copyright = bingsrc.images[0].copyright;
+      global.copyrightlink = bingsrc.images[0].copyrightlink;
+      global.title = bingsrc.images[0].title;
+    };
+  };
+};
 
-var job = new CronJob('0 0 * * *', downimg())
-job.start()
+var job = new CronJob('0 0 * * *', cacheimg());
+job.start();
 
-downimg()
+cacheimg();
 
 // 允许跨域
-app.all('*', function(req,
+app.all('*', function (req,
   res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Credentials","true");
-  if(req.method === "OPTIONS") res.send(200);
-  else  next();
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") res.send(200);
+  else next();
 });
- 
+
 app.get('/getimage', (req, res) => {
-  res.sendFile(path.join(__dirname, 'image.jpg'))
-})
+  res.sendFile(path.join(__dirname, 'image.jpg'));
+});
+
+app.get('/gettitle', (req, res) => {
+  res.send({
+    title: global.title
+  });
+});
+
+app.get('/getcopyright', (req, res) => {
+  res.send({
+    copyright: global.copyright,
+    copyrightlink: global.copyrightlink
+  })
+});
 
 app.listen(port, () => {
   console.log(`heStudio BingWallpaper Get is running on localhost:${port}`)
-})
+});
